3b51e56b09575c4608cc6467a81522df7b6d4cbd
Phan Vinh Khanh <Khanh161297@gmail.com>
2023-05-09 21:08:49 +0700

    Show data in Php

---
+++ BE/Lessons/Section-3/show_data.php
@@ -0,0 +1,11 @@
+<?php
+//show data with echo
+$a = "Phan Vinh Khanh";
+echo "My name is {$a}";
+
+// show data with print_r
+$my_array = array(1, 2, 3, 4, 5);
+echo "<pre>";
+print_r($my_array);
+print("</pre>");
+    ?>
