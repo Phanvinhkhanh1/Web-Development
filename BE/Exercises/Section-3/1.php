2074a6a809935427d956a8139a25edcfd91b3cfb
Phan Vinh Khanh <Khanh161297@gmail.com>
2023-05-09 21:45:19 +0700

    Exercise 1 in Section 3

---
+++ BE/Exercises/Section-3/1.php
@@ -0,0 +1,30 @@
+<?php
+$fullName = "Phan Vinh Khanh";
+$userName = "Khanh Phan";
+$email = "Khanh161297@gmail.com";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1 in Section 3</title>
+</head>
+
+<body>
+    <a href="#">Demo</a><br /><br />
+    <span>Họ và tên :
+        <?php echo $fullName; ?>
+    </span><br />
+    <span>Username :
+        <?php echo $userName; ?>
+    </span><br />
+    <span>Email :
+        <?php echo $email; ?>
+    </span>
+</body>
+
+</html>
