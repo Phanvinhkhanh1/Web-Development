dad9e9806f57c65d8fcea5cf8327c349a55250e8
Phan Vinh Khanh <Khanh161297@gmail.com>
2024-04-01 19:52:41 +0700

    Number in Php

---
+++ BE/Lessons/Section-4/number.php
@@ -0,0 +1,16 @@
+<?php 
+    // Integer number
+    $a = 100;
+    $b = 500;
+    echo $a;
+    echo "<br/>";
+    echo $b;
+
+    echo "<br/>";
+    // Real number
+    $c = 1.5;
+    $d = 5.6;
+    echo $c;
+    echo "<br/>";
+    echo $d;
+?>
