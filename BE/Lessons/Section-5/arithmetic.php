29a9563ab6167dcb6fb81754e637b998bd7a56e3
Phan Vinh Khanh <Khanh161297@gmail.com>
2024-04-01 20:24:55 +0700

    Arithmetic operator in PHP

---
+++ BE/Lessons/Section-5/arithmetic.php
@@ -0,0 +1,27 @@
+<?php 
+$a = 100;
+$b = 80;
+
+$total = $a + $b;
+echo "{$a} + {$b} = {$total}";
+echo "<br/>";
+
+$subtract = $a - $b;
+echo "{$a} - {$b} = {$subtract}";
+echo "<br/>";
+
+$multiply = $a * $b;
+echo "{$a} * {$b} = {$multiply}";
+echo "<br/>";
+
+$divide = $a / $b;
+echo "{$a} / {$b} = {$divide}";
+echo "<br/>";
+
+$remainder = $a % $b;
+echo "{$a} % {$b} = {$remainder}";
+echo "<br/>";
+
+$power = pow($a, $b);
+echo "{$a} ^ {$b} = {$power}";
+?>
