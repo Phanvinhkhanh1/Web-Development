10f35293ac9312d2d3737645e189c2017569f874
Phan Vinh Khanh <Khanh161297@gmail.com>
2024-04-01 20:04:30 +0700

    Array in Php

---
+++ BE/Lessons/Section-4/array.php
@@ -0,0 +1,22 @@
+<?php 
+// One dimensional array
+$a = array(0,3,8,9,10,56);
+echo "<pre>";
+print_r($a);
+echo "</pre>";
+
+// Multi-dimensional array
+$b = array(
+    1 => array(
+        "id" => 1,
+        "fullName" => "Phan Vinh Khanh"
+    ),
+    2 => array(
+        "id" => 2,
+        "fullName" => "Trinh Minh Anh"
+    )
+);
+echo "<pre>";
+print_r($b);
+echo "</pre>";
+?>
