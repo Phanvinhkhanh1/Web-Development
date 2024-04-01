a618e7bd35b90392cc8142fced9dc7546d131fdb
Phan Vinh Khanh <Khanh161297@gmail.com>
2024-04-01 20:28:24 +0700

    Assignment in PHP

---
+++ BE/Lessons/Section-5/assignment.php
@@ -0,0 +1,16 @@
+<?php 
+$a = 100;
+$b = 90;
+
+echo "a = {$a}";
+echo "<br/>";
+echo "b = {$b}";
+echo "<br/>";
+
+echo "============= After assignment ===========";
+echo "<br/>";
+
+$a += $b;
+echo "a = {$a}";
+echo "<br/>";
+?>
